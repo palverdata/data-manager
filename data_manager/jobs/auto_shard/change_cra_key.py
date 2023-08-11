4b66dd426f04e6634c658649a0f2ed6d0bd3e908
Giancarlo Rocha <giancarloiff@gmail.com>
2023-08-11 16:46:07 -0300

    feat: add job to flip shard keys for cra aliases

---
+++ data_manager/jobs/auto_shard/change_cra_key.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from data_manager.models.solr_config import SolrConfig
+from data_manager.solr import count_documents
+
+
+class ChangeCRAKeyJob:
+    """Change the CRA key for a given shard.
+
+    This job looks into every collection configured with 'cra_time' as the `shard_type`
+    and verifies that the threshold is not exceeded. If it is, it will change the shard
+    key to the current utc time in the format of 'YYYY-MM-DDTHH:MM:SSZ'. This will mark
+    the next shard key until the threshold is exceeded again.
+    """
+
+    def _change_cra_key(self, config: SolrConfig):
+        """Change the shard key for a given config."""
+
+        total = count_documents(config.current_shard_collection)
+
+        if total >= config.shard_threshold:
+            now = datetime.utcnow()
+
+            config.update(
+                shard_key=now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            )
+
+    def perform(self):
+        for config in SolrConfig.find({"shard_type": "cra_time"}):
+            self._change_cra_key(config)
